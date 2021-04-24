83135f51693629083d88db009c831cfe5dde0347
kyomukyomupurin <murakami1728@gmail.com>
2021-04-24 12:23:34 +0900

    :recycle: Refactor code

--- gen.py
+++ gen.py
@@ -7,10 +7,9 @@ from pathlib import Path
 def snippetize(filename: str) -> dict:
     snippet = {}
     snippet["prefix"] = Path(filename).stem
-    lines = Path(filename).read_text().splitlines()
-    body = []
-    processing = False
-
+    lines: list[str] = Path(filename).read_text().splitlines()
+    body: list[str] = []
+    processing: bool = False
     for line in lines:
         if line.startswith("// snippet-end"):
             break
@@ -18,25 +17,19 @@ def snippetize(filename: str) -> dict:
             processing = True
         elif processing:
             body.append(line)
-
     snippet["body"] = body
-
     return snippet
 
 
 if __name__ == "__main__":
     settings = json.load(Path("snippets_settings.json").open("r"))
-    target_files = []
-
+    target_files: list[str] = []
     for root, dirs, files in os.walk("."):
         for file_name in files:
             if Path(file_name).suffix in settings["extension"]:
                 target_files.append(join(root, file_name))
-
     snippets = {}
-
     for file in target_files:
         if snippetize(file)["body"]:
             snippets[Path(file).stem] = snippetize(file)
-
     Path(settings["output"]).write_text(json.dumps(snippets, indent=4))
